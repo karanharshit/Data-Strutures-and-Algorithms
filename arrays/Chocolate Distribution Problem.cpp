b5447c2ddf1989f759b7d3b7f79e3c1a1d55258f
Harshit Karan <75782432+karanharshit@users.noreply.github.com>
2020-12-12 01:06:35 +0530

    Update Chocolate Distribution Problem.cpp

--- arrays/Chocolate Distribution Problem.cpp
+++ arrays/Chocolate Distribution Problem.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n;
+	    int array[n];
+	    for(int i=0;i<n;i++){
+	        cin>>array[i];
+	    }
+	    cin>>m;
+	    sort(array,array+n);
+	    
+	    int A[n-m+1];
+	    for(int i=0;i<=n-m;i++){
+	        A[i]=array[i+m-1]-array[i];
+	    }
+	    
+	    int min=A[0];
+	    for(int i=1;i<=n-m;i++){
+	        if(A[i]<=min){
+	            min=A[i];
+	        }
+	    }
+	    cout<<min<<endl;
+	}
+	return 0;
+}
