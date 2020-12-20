1d12a72f1bd9bd8f9f46eab2bc112b17833bf03f
Harshit Karan <75782432+karanharshit@users.noreply.github.com>
2020-12-20 14:51:37 +0530

    Update print all permutations of a string.cpp

--- string/print all permutations of a string.cpp
+++ string/print all permutations of a string.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void permute(string a, int l, int r) 
+{ 
+	int i; 
+	if (l == r) 
+		cout<<a<<" "; 
+	else
+	{ 
+		for (i = l; i <= r; i++) 
+		{ 
+			swap(a[l], a[i]); 
+			permute(a, l+1, r); 
+			swap(a[l], a[i]); 
+		} 
+	} 
+} 
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--){
+	string str;
+	cin>>str;
+	int n = str.length(); 
+	permute(str, 0, n-1);
+	cout<<endl;
+	 }
+	 return 0;
+} 
