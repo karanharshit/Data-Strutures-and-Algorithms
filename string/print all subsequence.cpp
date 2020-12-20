6c616dafc20c630c8b87f951e125cea113e1d29d
Harshit Karan <75782432+karanharshit@users.noreply.github.com>
2020-12-20 14:04:35 +0530

    Update print all subsequence.cpp

--- string/print all subsequence.cpp
+++ string/print all subsequence.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+void func(string t, int i, int j, string s){
+	if(i==j){
+		cout<<t<<endl;
+	}
+	else{
+		func(t,i+1,j,s);
+		t=t+s[i];
+		func(t,i+1,j,s);
+	}
+}
+
+int main() {
+	string s;
+	cin>>s;
+	func("",0,s.length(),s);
+	return 0;
+}
