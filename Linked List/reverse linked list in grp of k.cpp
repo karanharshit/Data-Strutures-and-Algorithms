08569ec9d7af84a2b99579c5055481865e8acb47
Harshit Karan <75782432+karanharshit@users.noreply.github.com>
2021-01-22 15:14:01 +0530

    Create reverse linked list in grp of k.cpp

---
+++ Linked List/reverse linked list in grp of k.cpp
@@ -0,0 +1,18 @@
+node *prev=NULL;
+    node *curr=head;
+    node *next;
+    
+    int count=0;
+    while(curr!=NULL&&count<k){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count++;
+    }
+    
+    if(next!=NULL){
+        head->next=reverse(next,k);
+    }
+    
+    return prev;
