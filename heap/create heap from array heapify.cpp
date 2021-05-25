832bc4896874cda5015769e75e4265faa7f09da1
Harshit Karan <75782432+karanharshit@users.noreply.github.com>
2021-05-25 09:56:53 +0530

    Update create heap from array heapify.cpp

--- heap/create heap from array heapify.cpp
+++ heap/create heap from array heapify.cpp
@@ -1,64 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void heapify(int array[], int n, int i){
+    int largest=i;
+    int l=(2*i)+1;
+    int r=(2*i)+2;
+
+    if(l<n && array[l]>array[largest]){
+        largest=l;
+    }
+
+    if(r<n && array[r]>array[largest]){
+        largest=r;
+    }
+
+    if(largest!=i){
+        swap(array[i], array[largest]);
+        heapify(array,n,largest);
+    }
+}
+
+int main(){
     int n;
     cin>>n;
     int array[n];
     for(int i=0;i<n;i++){
         cin>>array[i];
     }
-    for(int i=n-1;i>=0;i--){
-        int l = 2*i + 1;
-        int r = 2*i + 2;
-        int largest = i;
-        if(l < n && array[l]>array[largest]){
-            int temp =  array[l];
-            array[l]=array[largest];
-            array[largest]=temp;
 
-             int l1 = 2*l + 1;
-             int r1 = 2*l + 2;
-             int largest1= l;
-
-             if(l1 < n && array[l1]>array[largest1]){
-               int temp =  array[l1];
-               array[l1]=array[largest1];
-               array[largest1]=temp;
-             }
-
-             if(r1 < n && array[r1]>array[largest1]){
-               int temp =  array[r1];
-               array[r1]=array[largest1];
-               array[largest1]=temp;
-             }
-        }
-
-        if(r < n && array[r]>array[largest]){
-            int temp =  array[r];
-            array[r]=array[largest];
-            array[largest]=temp;
-
-            int l1 = 2*r + 1;
-            int r1 = 2*r + 2;
-            int largest1= r;
-
-             if(l1 < n && array[l1]>array[largest1]){
-               int temp =  array[l1];
-               array[l1]=array[largest1];
-               array[largest1]=temp;
-             }
-
-             if(r1 < n && array[r1]>array[largest1]){
-               int temp =  array[r1];
-               array[r1]=array[largest1];
-               array[largest1]=temp;
-             }
-        }
+    for(int i=(n/2)-1;i>=0;i--){
+        heapify(array,n,i);
     }
 
     for(int i=0;i<n;i++){
         cout<<array[i]<<" ";
     }
-
 }
